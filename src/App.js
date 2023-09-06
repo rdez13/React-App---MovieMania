2441e64527261038317b37f08c78153003ea2f29
rdez13 <ryandezfuli@yahoo.com>
2023-09-05 23:26:40 -0400

    changed typo to make movies appera, changed apikey

--- src/App.js
+++ src/App.js
@@ -1,27 +1,39 @@
-import { useState, useEffect } from 'react';
-
+import React, { useState, useEffect } from 'react';
 import MovieCard from './MovieCard';
-import './App.css';
 import SearchIcon from './search.svg';
+import './App.css';
 
-const API_URL = 'http://www.omdbapi.com?apikey='
+
+const API_URL = 'http://www.omdbapi.com?apikey=4c0a9785';
+
 
 
 const App = () => {
-    const [movies, setMovies] = useState([]);
     const [searchTerm, setSearchTerm] = useState('');
+    const [movies, setMovies] = useState([]);
 
-    useEffect(() => {
-        searchMovies('Iron Man');
-    }, []);
+   
 
     const searchMovies = async (title) => {
         //call to api
-        const response = await fetch(`${API_URL}&s=${title}`);
+        const completeURL = API_URL + "&s=" + title;
+        const response = await fetch(completeURL);
+        //(`${API_URL}&s=${title}`);
         const data = await response.json();
-
-        setMovies(data.search);
+        console.log(data)
+        setMovies(data.Search);
     };
+
+    const handleKeyPress = (event) => {
+        if (event.key === "Enter") {
+        searchMovies(searchTerm);
+        }
+      };
+
+    useEffect(() => {
+        searchMovies("iron man");
+    }, [])
+
    
 
     return(
@@ -30,9 +42,10 @@ const App = () => {
 
            <div className="search">
             <input 
-                placeholder="Search for movies"
                 value={searchTerm}
                 onChange={(e) => setSearchTerm(e.target.value)}
+                onKeyDown={handleKeyPress}
+                placeholder="Search for movies"
                 />
             <img 
                 src={SearchIcon}
@@ -41,20 +54,19 @@ const App = () => {
             />
            </div>
 
-            {movies?.length > 0
-                ? (
-                    <div className="container">
-                        {movies.map((movie) => (
-                            <MovieCard movie={movie} />
-                        ))}
-                    </div>
-                ) : (
-                    <div className="empty">
-                        <h2>No movies found</h2>
-                    </div>
-                )}
+            {movies?.length > 0 ?(
+                <div className="container">
+                    { movies.map((movie) => {
+                        return <MovieCard movie={movie} />
+                    })}
+                </div>
+            ) : (
+                <div className="empty">
+                    <h2>No movies found</h2>                    
+                </div>
+            )}
         </div>
     );
-}
+};
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-
+//import ReactDOM from 'react-dom/client';
+import { createRoot } from 'react-dom/client'
 import App from './App';
-ReactDOM.render(<App />, document.getElementById('root'));
+
+createRoot(document.getElementById('root')).render(<App />)
+
+//ReactDOM.createRoot(<App />, document.getElementById('root'));
+//ReactDOM.render(<App />, document.getElementById('root'));
